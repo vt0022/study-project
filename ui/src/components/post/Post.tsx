b7dfbb3d3ceb8cd2f62524d9687f09ebbbd90607
Nguyen Van Thuan <vanthuan2004@gmail.com>
2025-03-20 23:34:18 +0700

    Build UI for post functionality

--- ui/src/components/post/Post.tsx
+++ ui/src/components/post/Post.tsx
@@ -1,18 +1,26 @@
-import { Avatar, Box, Grid2 } from "@mui/material";
+import { Avatar, Box, Grid2, Stack, Typography } from "@mui/material";
 
 function Post() {
-  return (<Box>
-    <Grid2 container>
-      <Grid2 size={2}>
-        <Avatar alt="Other avatar" sx={{ width: "50px", height: "50px", margin:'0 auto' }}>Anna</Avatar>
-      </Grid2>
+    return (
+        <Box>
+            <Grid2 container spacing={3}>
+                <Grid2>
+                    <Avatar alt="Other avatar" sx={{ width: "50px", height: "50px", margin: "0 auto" }}>
+                        Anna
+                    </Avatar>
+                </Grid2>
 
-      <Grid2 size={10}>
-        <h1>Body</h1>
-      </Grid2>
-    </Grid2>
-    
-  </Box>);
+                <Grid2 size="grow">
+                    <Stack spacing={1}>
+                        <Typography sx={{ fontWeight: "bold", textAlign: "justify" }}>10/12/2002 16:10</Typography>
+                        <Typography variant="body1" sx={{ textAlign: "justify" }}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+                        </Typography>
+                    </Stack>
+                </Grid2>
+            </Grid2>
+        </Box>
+    );
 }
 
 export default Post;
--- ui/src/pages/post/Home.tsx
+++ ui/src/pages/post/Home.tsx
@@ -1,48 +1,82 @@
 import Post from "@/components/post/Post";
 import { usePrivateAxios } from "@/hooks/usePrivateAxios";
-import { Avatar, Box, Stack, TextField, Typography } from "@mui/material";
+import { Image, ImageOutlined } from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
+import { Avatar, Box, Button, Grid2, IconButton, Stack, TextField, Typography } from "@mui/material";
+
+const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+});
 
 function Home() {
-  usePrivateAxios();
+    usePrivateAxios();
 
-  return (
-    <Stack
-      spacing="10px"
-      sx={{
-        backgroundColor: "#f0f0f0",
-        marginLeft: "400px",
-        flexGrow: 1,
-        paddingX: "10px",
-      }}
-    >
-      <Box sx={{ backgroundColor: "white", padding: "20px" }}>
-        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-          Home
-        </Typography>
-      </Box>
+    return (
+        <Stack
+            spacing="10px"
+            sx={{
+                backgroundColor: "#f0f0f0",
+                marginLeft: "400px",
+                flexGrow: 1,
+                paddingX: "10px",
+            }}>
+            <Box sx={{ backgroundColor: "white", padding: "20px" }}>
+                <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+                    Home
+                </Typography>
+            </Box>
 
-      <Box sx={{ backgroundColor: "white", p: "10px" }}>
-        <Stack direction="row" sx={{ margin: "20px" }} spacing={5}>
-          <Avatar alt="My avatar" sx={{ width: "50px", height: "50px" }}>
-            Me
-          </Avatar>
+            <Box sx={{ backgroundColor: "white", p: "20px" }}>
+                <Grid2 container spacing={3}>
+                    <Grid2>
+                        <Avatar alt="My avatar" sx={{ width: "50px", height: "50px" }}>
+                            Me
+                        </Avatar>
+                    </Grid2>
 
-          <TextField
-            id="standard-multiline-flexible"
-            label="What is on your mind?"
-            multiline
-            maxRows={10}
-            variant="standard"
-            fullWidth
-          />
+                    <Grid2 size="grow">
+                        <Stack spacing={2}>
+                            <TextField
+                                id="standard-multiline-flexible"
+                                label="What is on your mind?"
+                                multiline
+                                maxRows={10}
+                                variant="standard"
+                                fullWidth
+                                slotProps={{
+                                    input: { sx: { fontSize: "25px" } }, // ✅ Tăng cỡ chữ input
+                                    inputLabel: { sx: { fontSize: "20px" } }, // ✅ Tăng cỡ chữ label
+                                }}
+                            />
+
+                            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                                <IconButton aria-label="Upload image" color="primary" component="label">
+                                    <ImageOutlined />
+                                    <VisuallyHiddenInput type="file" onChange={(event) => console.log(event.target.files)} />
+                                </IconButton>
+
+                                <Button variant="contained" sx={{ borderRadius: "1000px" }}>
+                                    Post
+                                </Button>
+                            </Box>
+                        </Stack>
+                    </Grid2>
+                </Grid2>
+            </Box>
+
+            <Box sx={{ backgroundColor: "white", padding: "20px" }}>
+                <Post />
+            </Box>
         </Stack>
-      </Box>
-
-      <Box sx={{ backgroundColor: "white", padding: "10px" }}>
-        <Post />
-      </Box>
-    </Stack>
-  );
+    );
 }
 
 export default Home;
